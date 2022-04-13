6c065d9d03b65b3753f61bbf1ea5bc8494d9994e
Suraj <suraj756@gmail.com>
2022-04-13 06:22:11 +0530

    Initial Commit

---
+++ TestBoilerplateApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBoilerplateApp
+//
+//  Created by SAISURAJ on 4/13/22.
+//  Copyright © 2022 SAISURAJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
