2d8e182cb50ad605fd8ab8b7063d0076b08ca87e
Suraj <corpussai123@gmail.com>
2022-04-18 07:07:07 +0530

    added print

--- TestBoilerplateApp/ViewController.swift
+++ TestBoilerplateApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        ServerConfig.config.setServerConfig()
+        print("added print")
         // Do any additional setup after loading the view.
     }
 
